02984937cfa5ef927a8480a55dd49b96e9946ba0
Johanna Gibbs <johanna.gibbs17@gmail.com>
2021-05-17 11:18:41 +0300

    js fix

--- pages/time.js
+++ pages/time.js
@@ -2,24 +2,19 @@ var today = new Date();
 var hours = today.getUTCHours();
 
 
-
 let day = document.querySelector('.day')
 let night = document.querySelector('.night')
 
-
-
-if (hours > 5 || hours < 13) {
+if (hours >= 5 && hours <= 13) {
     day.style.display = "block";
     night.style.display = "none";
-   
 }
 
-if (hours < 5 || hours > 13) {
+else {
     day.style.display = "none";
     night.style.display = "block";
     
     let blurred = document.querySelector('h1')
     blurred.classList.add('blur');
-
 }
 
